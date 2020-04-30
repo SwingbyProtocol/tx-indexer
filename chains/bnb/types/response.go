4190716e6bb3f9b4916f0daf9fdac892a7ae6d01
syrohei <syrohei@gmail.com>
2020-04-30 15:36:06 +0800

    Fix response.go

--- chains/bnb/types/response.go
+++
@@ -1,9 +0,0 @@
-package types
-
-type TransactionsRes struct {
-	Txs []Transaction `json:"txArray"`
-}
-type MultiSendTransactionsRes struct {
-	Txs   []Transaction `json:"subTxDtoList"`
-	Total int           `json:"totalNum"`
-}
