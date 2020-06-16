475081b02c32cf421da933862a889843f6b938da
syrohei <syrohei@gmail.com>
2020-06-16 14:57:06 +0800

    Fix configs

--- README.md
+++ README.md
@@ -20,11 +20,9 @@ $ go run ./cmd/tx-indexer --btc.node http://192.168.1.101:8332 --btc.testnet
       --eth.node string      The address for connect eth fullnode (default "http://192.168.1.230:8332")
       --eth.testnet          This is a eth testnet (Goerli)
       --eth.token string     The address for watch address (default "0xaff4481d10270f50f203e0763e2597776068cbc5")
-      --eth.watch string     The address for watch address (default "0x3Ec6671171710F13a1a980bc424672d873b38808")
       --log.level string     The log level (default "info")
   -l, --rest.listen string   The listen address for REST API (default "0.0.0.0:9096")
   -w, --ws.listen string     The listen address for Websocket API (default "0.0.0.0:9099")
-
 ```
 ## Build 
 ```
--- config/config.go
+++ config/config.go
@@ -8,14 +8,10 @@ import (
 )
 
 const (
-	DefaultBTCNode         = "http://192.168.1.230:8332"
-	DefautlBTCWatchAddress = "mr6ioeUxNMoavbr2VjaSbPAovzzgDT7Su9"
-	DefaultETHNode         = "http://192.168.1.230:8332"
-	DefautlETHWatchAddress = "0x3Ec6671171710F13a1a980bc424672d873b38808"
-	DefaultERC20Token      = "0xaff4481d10270f50f203e0763e2597776068cbc5"
-	DefaultBNCNode         = "tcp://192.168.1.146:26657"
-	DefautlBNCWatchAddress = "tbnb1ws2z8n9ygrnaeqwng69cxfpnundneyjze9cjsy"
-	DefaultAccessToken     = "swingbyswingby"
+	DefaultBTCNode    = "http://192.168.1.230:8332"
+	DefaultETHNode    = "http://192.168.1.230:8332"
+	DefaultERC20Token = "0xaff4481d10270f50f203e0763e2597776068cbc5"
+	DefaultBNCNode    = "tcp://192.168.1.146:26657"
 )
 
 // Config is app of conig
@@ -73,9 +69,6 @@ func init() {
 	pflag.String("btc.node", DefaultBTCNode, "The address for connect btc fullnode")
 	// The target number of outbound peers. Defaults to 25.
 	pflag.Int("btc.nodeSize", 25, "The maximum node count for connect p2p")
-	// The watch address
-	pflag.String("btc.watch", DefautlBTCWatchAddress, "The address for watch address")
-
 	// Set ETH config
 	pflag.String("eth.node", DefaultETHNode, "The address for connect eth fullnode")
 
@@ -87,8 +80,6 @@ func init() {
 	pflag.String("bnc.node", DefaultBNCNode, "The address for connect bnc fullnode")
 
 	pflag.Bool("bnc.testnet", false, "This is a bnc testnet")
-
-	pflag.String("bnc.watch", DefautlBNCWatchAddress, "This is a bnc testnet")
 
 	pflag.StringP("rest.listen", "l", "0.0.0.0:9096", "The listen address for REST API")
 	// Bind ws flags
